279076e684953a727c762dfbc61160a5177462bd
Sergio Maldonado <plastikinsekt@aim.com>
2022-07-12 21:08:20 -0500

    Adds SafeAreaView to TabBar.

--- src/components/TabBar/index.js
+++ src/components/TabBar/index.js
@@ -1,32 +1,35 @@
 import React from 'react'
+import { SafeAreaView } from 'react-native'
 import CalendarIcon from '../../assets/icons/calendario-mas.svg'
 import HomeIcon from '../../assets/icons/casa.svg'
 import MyPikIcon from '../../assets/icons/mypik-isotipo.svg'
 import TabBarItem from '../TabBarItem'
 import * as Components from './style'
 
-const _Template = () => {
+const TabBar = () => {
   return (
     <Components.Container>
-      <Components.ItemsContainer>
-        <TabBarItem
-          icon={<HomeIcon width={25} height={25} />}
-          title="Inicio"
-          onPress={() => alert('Home button pressed.')}
-        />
-        <TabBarItem
-          icon={<MyPikIcon width={24} height={30} />}
-          title="Inicio"
-          onPress={() => alert('MyPik button pressed.')}
-        />
-        <TabBarItem
-          icon={<CalendarIcon width={25} height={25} />}
-          title="Inicio"
-          onPress={() => alert('Calendar button pressed.')}
-        />
-      </Components.ItemsContainer>
+      <SafeAreaView>
+        <Components.ItemsContainer>
+          <TabBarItem
+            icon={<HomeIcon width={25} height={25} />}
+            title="Inicio"
+            onPress={() => alert('Home button pressed.')}
+          />
+          <TabBarItem
+            icon={<MyPikIcon width={24} height={30} />}
+            title="Inicio"
+            onPress={() => alert('MyPik button pressed.')}
+          />
+          <TabBarItem
+            icon={<CalendarIcon width={25} height={25} />}
+            title="Inicio"
+            onPress={() => alert('Calendar button pressed.')}
+          />
+        </Components.ItemsContainer>
+      </SafeAreaView>
     </Components.Container>
   )
 }
 
-export default _Template
+export default TabBar
