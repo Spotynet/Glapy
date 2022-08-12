adadffd70012ffb95e989ae4f9bb959321b56d95
Sergio Maldonado <plastikinsekt@aim.com>
2022-08-11 22:24:29 -0500

    Adds categories icons (embedded).

--- src/components/CategoriesBarItem/index.js
+++ src/components/CategoriesBarItem/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import * as Components from './style'
+import { Image } from 'react-native'
+import { useTheme } from 'styled-components'
 
-const CategoriesBarItem = ({ active, title }) => {
+const CategoriesBarItem = ({ active, item }) => {
+  const theme = useTheme()
+
   return (
     <Components.Container>
-      <Components.Title>{title}</Components.Title>
-      {active && <Components.Indicator />}
+      <Image
+        source={active ? item.imageActive : item.image}
+        resizeMode={'contain'}
+        width={30}
+        height={30}
+      />
+      <Components.Title theme={theme} active={active}>
+        {item.name}
+      </Components.Title>
+      {/* {active && <Components.Indicator />} */}
     </Components.Container>
   )
 }
